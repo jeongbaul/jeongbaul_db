bc90e6db45d8ea503cd77a3bd64fd982120de824
jeongbaul <bauljeong07@gmail.com>
2025-01-22 22:40:51 +0900

    Create select.php

---
+++ htdocs/select.php
@@ -0,0 +1,31 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'].'/lib/dbconnect.php';
+include $_SERVER['DOCUMENT_ROOT'].'/lib/db.php';
+
+// Read
+$user = read('employees', ['first_name' => 'Georgi'], 'emp_no DESC', '10');
+foreach ($user as $user) {
+    echo "ID: {$user['emp_no']}, first_name: {$user['first_name']}, last_name: {$user['last_name']}<br/>";
+}
+?>
+
+<table border="1">
+    <tr>
+        <th>회원번호</th>
+        <th>이름</th>
+        <th>성</th>
+    </tr>
+<?php
+// Read
+$user = read('employees',['first_name' => 'Georgi'], 'emp_no DESC', '10');
+foreach ($user as $user) {
+?>
+    <tr>
+    <td><?php echo $user['emp_no']; ?></td>
+    <td><?php echo $user['first_name']; ?></td>
+    <td><?php echo $user['last_name']; ?></td>
+    </tr>
+    <?php
+}
+?>
+</table>
