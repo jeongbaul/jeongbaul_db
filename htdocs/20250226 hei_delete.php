f301a09f3754fc553000491583a9045cc15e681d
jeongbaul <bauljeong07@gmail.com>
2025-02-26 02:33:45 +0900

    Create 20250226 hei_delete.php
    하이델베르크 요리문답에서 삭제를 하기 위한 delete문

---
+++ htdocs/20250226 hei_delete.php
@@ -0,0 +1,24 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'].'/lib/dbconnect.php';
+include $_SERVER['DOCUMENT_ROOT'].'/lib/db.php';
+
+if (isset($_GET['week'])) {
+    $week = $_GET['week'];
+
+    // Delete from prayer table
+    $deleteConditions = ['week' => $week];
+    $result = delete('prayer', $deleteConditions);
+    
+    if ($result) {
+        echo "해당 주차의 질문과 답변이 성공적으로 삭제되었습니다.";
+    } else {
+        echo "삭제에 실패했습니다.";
+    }
+}
+?>
+
+<form method="GET" action="delete.php">
+    <label for="week">삭제할 주차:</label>
+    <input type="number" id="week" name="week" required>
+    <input type="submit" value="삭제">
+</form>
