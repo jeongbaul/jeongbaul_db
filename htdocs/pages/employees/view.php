0ca8e5d695d1db2e14ec4faacd8a8c273c9bc3f9
jeongbaul <bauljeong07@gmail.com>
2025-05-13 01:04:25 +0900

    20250513
    후.... 성공이였으면 좋겠당

--- htdocs/pages/employees/view.php
+++ htdocs/pages/employees/view.php
@@ -2,22 +2,34 @@
 include $_SERVER['DOCUMENT_ROOT'].'/lib/dbconnect.php';
 include $_SERVER['DOCUMENT_ROOT'].'/lib/db.php';
 
+// 사원 정보를 조회
 $user = read('employees', ['emp_no' => $_GET['emp_no']], '', '1');
+
+// 사진 경로 및 파일명 불러오기
+$photo = read('photo', ['emp_no' => $_GET['emp_no']], '', '1');  // emp_no로 사진 정보 조회
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>사원 상세</title>
 </head>
 <body>
 <h1>사원상세</h1>
-    FirstName:<?php echo $user[0]['first_name']; ?><br />
-    LastName:<?php echo $user[0]['last_name']; ?><br />
-    BirthDate:<?php echo $user[0]['birth_date']; ?><br />
-    Gender:<?php echo $user[0]['gender']; ?><br />
-    HireDate:<?php echo $user[0]['hire_date']; ?><br />
+    FirstName: <?php echo $user[0]['first_name']; ?><br />
+    LastName: <?php echo $user[0]['last_name']; ?><br />
+    BirthDate: <?php echo $user[0]['birth_date']; ?><br />
+    Gender: <?php echo $user[0]['gender']; ?><br />
+    HireDate: <?php echo $user[0]['hire_date']; ?><br />
+
+    <!-- 사진이 있으면 출력, 없으면 기본 이미지 출력 -->
+    <?php if ($photo): ?>
+        <img src="/<?php echo $photo[0]['path'] . $photo[0]['file_name']; ?>" alt="Profile Picture" width="100" height="100" /><br />
+    <?php else: ?>
+        <img src="/default-photo.jpg" alt="Default Profile Picture" width="100" height="100" /><br />
+    <?php endif; ?>
+
     <button onclick="location.replace('list')">목록으로</button>
 </body>
 </html>
